740d40f389748ef3103af60b9ac0627f6713a631
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2026-01-14 06:19:41 -0500

    shader tweaks

--- fragment.glsl
+++ fragment.glsl
@@ -11,19 +11,22 @@ void main() {
     float delta = 0.003;
 
     vec4 base = texture2D(tDiffuse, vUv);
+
+    // edge detection
     vec4 c1 = texture2D(tDiffuse, vUv + vec2(delta, 0.0));
     vec4 c2 = texture2D(tDiffuse, vUv + vec2(-delta, 0.0));
     vec4 c3 = texture2D(tDiffuse, vUv + vec2(0.0, delta));
     vec4 c4 = texture2D(tDiffuse, vUv + vec2(0.0, -delta));
 
-    vec4 color = (c2 - c1) + (c4 - c3) + base * 0.5;
+    vec4 color = (c2 - c1) + (c4 - c3) + base * 0.4;
 
     color *= vec4(0.2, 1.0, 0.3, 1.0);
 
-    float pixelator = floor(mod(vUv.y * 200.0,2.0)) * floor(mod(vUv.x * 200.0, 2.0));
+    float pixelator = floor(mod(vUv.y * 300.0,2.0)) * floor(mod(vUv.x * 300.0, 2.0));
 
-    color *= 0.5 + pixelator;
-    //color += 0.1 * vec4(0.2, 1.0, 0.3, 1.0) * pixelator;
+    color *= 0.2 + pixelator;
+
+    color *= 1.0 + 0.2 * cos(vUv.y * 100.0 + time * 0.1);
 
     color.a = 1.0;
 
